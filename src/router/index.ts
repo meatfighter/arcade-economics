33b92b1be1399e43c548212dc15605d34f4f3eef
Michael Birken <o__1@hotmail.com>
2024-03-22 15:04:21 -0400

    creating

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,20 @@
-/**
- * router/index.ts
- *
- * Automatic routes for `./src/pages/*.vue`
- */
-
-// Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
-import { setupLayouts } from 'virtual:generated-layouts'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  extendRoutes: setupLayouts,
-})
+  routes: [
+      {
+        path: '/',
+        component: () => import('@/layouts/Default.vue'),
+        children: [
+          {
+            path: '',
+            name: 'Main',
+            component: () => import('@/views/Main.vue'),
+          },
+        ],
+      },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/Main.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ArcadeTable from "@/components/ArcadeTable.vue";
+</script>
+
+<template>
+  <arcade-table/>
+</template>
