ba526ec2653daad19ed683107bf5add3f074154e
Michael Birken <o__1@hotmail.com>
2024-03-24 23:20:48 -0400

    centered

---
+++ src/components/MainTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row align="center">
+      <v-col cols="auto">
+        <v-img
+            src="@/assets/insert-coin.svg"
+            class="ma-5"
+            contain
+            min-width="100px"
+            max-width="100px"
+            alt="Insert Coin"
+        ></v-img>
+      </v-col>
+      <v-col>
+        <div class="text-h3 mb-2">Retro Gaming, Modern Money</div>
+        <div class="text-subtitle-1">How Much Would That Quarter Cost You Today?</div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 import ArcadeTable from "@/components/ArcadeTable.vue";
+import MainTitle from "@/components/MainTitle.vue";
 </script>
 
 <template>
-    <p class="text-left">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.
-    </p>
-    <p class="text-left">
-      Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.
-    </p>
+    <main-title/>
     <arcade-table/>
 </template>
 
