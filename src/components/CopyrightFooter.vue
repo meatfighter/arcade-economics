872889ba355c1c5ce210811252400e698a37d9f0
Michael Birken <o__1@hotmail.com>
2025-01-19 14:49:27 -0500

    update

--- src/components/CopyrightFooter.vue
+++ src/components/CopyrightFooter.vue
@@ -2,7 +2,7 @@
   <v-container fluid class="px-2">
     <v-row>
       <v-col class="text-caption">
-        <div>&copy; 2024 meatfighter.com</div>
+        <div>&copy; 2025 meatfighter.com</div>
         <div>This content is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" rel="license noopener noreferrer" style="display:inline-block;">CC BY-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt="CC"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt="BY"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt="SA"></a></div>
       </v-col>
       <v-col cols="auto" align-self="center" class="text-right">
